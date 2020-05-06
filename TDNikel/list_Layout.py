4b9ee401060a0db0ea566db56819639dd1a57553
MikeNordMan <49362861+MikeNordMan@users.noreply.github.com>
2020-05-06 13:22:06 +0500

    Add files via upload

--- TDNikel/list_Layout.py
+++ TDNikel/list_Layout.py
@@ -6,13 +6,20 @@ import PySimpleGUI as sg
 from marki import names
 
 myRow = 15  # Колличество строк которые будут появляться
+lStrText = 12 # длинна текстового поля
+lenRasdelLine = 75 # Длинна разделительной линии
 
 def new_Admission ():
     #myRow = 15  # Колличество строк которые будут появляться
     '''Часть Layout отвечающаяя за заголовок'''
 
-    heder = [[sg.Text('Дата поступления:', pad=(15, 0)), sg.InputText(key='data', pad=(0, 0))],
-             [sg.Text('Номер а/м', pad=(15, 10)), sg.InputText(key='auto', pad=(47, 10))]]
+    heder = [
+             [sg.Text('Дата поступления:', pad=(5, 0)), sg.InputText(key='data', pad=(60, 0), size=(15, 1))],
+             [sg.Text('Транспорт (Марка, номер)', pad=(5, 0)), sg.InputText(key='auto', pad=(15, 10), size=(15, 1))],
+             [sg.Text('_' * lenRasdelLine)],  # Разделительная линия
+             [sg.Text('Наименование', size=(lStrText, 1)), sg.Text('Вес Нетто (кг)', size=(lStrText, 1)),
+              sg.Text('Засор ( % )', size=(lStrText, 1)) , sg.Text('Вес Брутто (кг)', size=(lStrText, 1))]
+            ]
 
     '''Часть Layout отвечающая за формирование строки сопроводительной '''
     mol = [[[sg.InputCombo(names, size=(20, 1), key=str(i + 1)), sg.InputText(key='in' + str(i + 1), size=(20, 1)),
@@ -20,10 +27,14 @@ def new_Admission ():
              sg.Text(background_color='white', key='res' + str(i + 1), size=(10, 1), enable_events=True,
                      text_color='black')]] for i in range(myRow)]
 
-    colButton = [[sg.Button('Добавить строку', key='on', visible=True),
+    colButton = [
+                 [sg.Text('_' * lenRasdelLine)],   # Разделительная линия
+                 [sg.Button('Добавить строку', key='on', visible=True),
                   sg.Button('Удалить строку', key='off', visible=True),
                   sg.Button('Сохранить', key='save', visible=True),
-                  sg.Button('Печать', key='printF')]]
+                  sg.Button('Печать', key='printF')],
+                 [sg.Text('Иформационная строка', text_color='red', pad=(0, 10))]
+                ]
 
     exitMesagge = [[sg.Text('', key='txt', size=(40, 2))]]
 
